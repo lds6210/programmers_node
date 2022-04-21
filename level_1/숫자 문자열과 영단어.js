af94c5e9bf4690cb7b04e6b314511a55c25e425f
Lee <dongsub.lee@medistream.co.kr>
2022-04-22 01:49:23 +0900

    숫자 문자열과 영단어

---
+++ level_1/숫자 문자열과 영단어.js
@@ -0,0 +1,43 @@
+s = "2three45sixseven";
+
+// a를 b로 모두 바꿔줘
+// function solution(s) {
+//   var answer = 0;
+//   answer = s
+//     .replace(/zero/g, "0")
+//     .replace(/one/g, "1")
+//     .replace(/two/g, "2")
+//     .replace(/three/g, "3")
+//     .replace(/four/g, "4")
+//     .replace(/five/g, "5")
+//     .replace(/six/g, "6")
+//     .replace(/seven/g, "7")
+//     .replace(/eight/g, "8")
+//     .replace(/nine/g, "9");
+//   return answer;
+// }
+
+function solution(s) {
+  let numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  var answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
+
+console.log(solution(s));
