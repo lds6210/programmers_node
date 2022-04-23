47b5d66b97fcecef8519efb9061568446889c922
Lee <dongsub.lee@medistream.co.kr>
2022-04-24 01:30:19 +0900

    두 개 뽑아서 더하기

---
+++ level_1/두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+var numbers = [2, 1, 3, 4, 1];
+console.log(solution(numbers));
+
+function solution(numbers) {
+  const temp = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      temp.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  const answer = [...new Set(temp)];
+
+  return answer.sort((a, b) => a - b);
+}
